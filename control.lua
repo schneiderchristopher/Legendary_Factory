70d2c676ae6d1e9ef3917c715ecd46d536f5c6d1
Christopher Schneider <schneiderchristopher16@gmail.com>
2024-11-20 05:52:06 -0300

    initial commit

---
+++ control.lua
@@ -0,0 +1,15 @@
+script.on_init(function()
+  local technologies_to_research = {
+	'quality-module',
+    'epic-quality',
+	'legendary-quality'
+  }
+
+  for _, tech_name in ipairs(technologies_to_research) do
+    for _, force in pairs(game.forces) do
+      if force.technologies[tech_name] then
+        force.technologies[tech_name].researched = true
+      end
+    end
+  end
+end)
