8ee4c9c02af639e300bbe3d57ebaca74131c713c
Christopher Schneider <schneiderchristopher16@gmail.com>
2024-11-21 04:17:54 -0300

    feat: improves code, and allow normal burner miner drill

--- control.lua
+++ control.lua
@@ -16,54 +16,53 @@ script.on_init(function()
     end
   end
 end)
-script.on_event({defines.events.on_cutscene_cancelled, defines.events.on_cutscene_finished}, function(event)
+script.on_event({ defines.events.on_cutscene_cancelled, defines.events.on_cutscene_finished }, function(event)
   setup_player_initial_items(event)
 end)
 
-script.on_event({defines.events.on_player_created, defines.events.on_player_joined_game}, function(event)
+script.on_event({ defines.events.on_player_created, defines.events.on_player_joined_game }, function(event)
   setup_player_initial_items(event)
 end)
 
 function setup_player_initial_items(event)
   local player = game.get_player(event.player_index)
-  
+
   if (player) then
-  if Contains(player_list,player) then return end
-  if (player.controller_type ~= defines.controllers.character) then return end
-  local player_inventory = player.character.get_inventory(defines.inventory.character_main)
+    if Contains(player_list, player) then return end
+    if (player.controller_type ~= defines.controllers.character) then return end
+    local player_inventory = player.character.get_inventory(defines.inventory.character_main)
 
-  if player_inventory then
-    player_inventory.clear()
+    if player_inventory then
+      player_inventory.clear()
 
-    local difficulty = settings.startup["difficulty"].value
-    if difficulty == "Default" then
-      player_inventory.insert({ name="stone-furnace", quality="legendary", count=2})
-      player_inventory.insert({ name="burner-mining-drill", quality="legendary", count=4})
-      player_inventory.insert({ name="burner-recycler", quality="legendary", count=1})
-      player_inventory.insert({ name="burner-assembling-machine", quality="legendary", count=1})
-      player_inventory.insert({ name="wooden-chest", quality="legendary", count=1})
+      local difficulty = settings.startup["difficulty"].value
+      if difficulty == "Default" then
+        player_inventory.insert({ name = "stone-furnace", quality = "legendary", count = 2 })
+        player_inventory.insert({ name = "burner-mining-drill", count = 1})
+        player_inventory.insert({ name = "burner-recycler", quality = "legendary", count = 1 })
+        player_inventory.insert({ name = "burner-assembling-machine", quality = "legendary", count = 1 })
+        player_inventory.insert({ name = "wooden-chest", quality = "legendary", count = 1 })
+      end
+      if difficulty == "Hard" then
+        player_inventory.insert({ name = "stone-furnace", quality = "legendary", count = 2 })
+        player_inventory.insert({ name = "burner-mining-drill", count = 1})
+        player_inventory.insert({ name = "burner-recycler", quality = "legendary", count = 1 })
+        player_inventory.insert({ name = "burner-assembling-machine", quality = "legendary", count = 1 })
+        player_inventory.insert({ name = "wooden-chest", quality = "legendary", count = 1 })
+      end
+      if difficulty == "Very Hard" then
+        player_inventory.insert({ name = "stone-furnace", quality = "legendary", count = 1 })
+        player_inventory.insert({ name = "burner-mining-drill", count = 1})
+        player_inventory.insert({ name = "burner-recycler", quality = "legendary", count = 1 })
+        player_inventory.insert({ name = "burner-assembling-machine", quality = "legendary", count = 1 })
+        player_inventory.insert({ name = "wooden-chest", quality = "legendary", count = 1 })
+      end
+      if difficulty == "Masochist" then
+        player_inventory.insert({ name = "stone-furnace", quality = "legendary", count = 1 })
+        player_inventory.insert({ name = "burner-mining-drill", count = 1})
+        player_inventory.insert({ name = "burner-assembling-machine", quality = "legendary", count = 1 })
+      end
     end
-    if difficulty == "Hard" then
-      player_inventory.insert({ name="stone-furnace", quality="legendary", count=2})
-      player_inventory.insert({ name="burner-mining-drill", quality="legendary", count=2})
-      player_inventory.insert({ name="burner-recycler", quality="legendary", count=1})
-      player_inventory.insert({ name="burner-assembling-machine", quality="legendary", count=1})
-      player_inventory.insert({ name="wooden-chest", quality="legendary", count=1})
-    end
-    if difficulty == "Very Hard" then
-      player_inventory.insert({ name="stone-furnace", quality="legendary", count=1})
-      player_inventory.insert({ name="burner-mining-drill", quality="legendary", count=1})
-      player_inventory.insert({ name="burner-recycler", quality="legendary", count=1})
-      player_inventory.insert({ name="burner-assembling-machine", quality="legendary", count=1})
-      player_inventory.insert({ name="wooden-chest", quality="legendary", count=1})
-    end
-    if difficulty == "Masochist" then
-      player_inventory.insert({ name="stone-furnace", quality="legendary", count=1})
-      player_inventory.insert({ name="burner-mining-drill", quality="legendary", count=1})
-      player_inventory.insert({ name="burner-assembling-machine", quality="legendary", count=1})
-    end
-  end
-  
   end
 end
 local ignore_group = {
@@ -127,6 +126,35 @@ local function destroy_not_legendary(player, entity, game, item)
   end
 end
 
+--- @param entity LuaEntity
+--- @param group (string) Either "group" or "subgroup". Determines the operation.
+--- @return string
+function getGroupName(entity, group)
+  ---@type string
+  local group_name = nil
+  if (entity.prototype.group.name == 'other') then
+    group_name = entity.ghost_prototype[group].name
+  else
+    group_name = entity.prototype[group].name
+  end
+  assert(group_name ~= nil, "Error setting group name")
+  return group_name
+end
+
+--- @param entity LuaEntity
+--- @return string
+function getEntityName(entity)
+  ---@type string
+  local name = nil
+  if (entity.prototype.group.name == 'other') then
+    name = entity.ghost_name
+  else
+    name = entity.name
+  end
+  assert(name ~= nil, "Error setting group name")
+  return name
+end
+
 script.on_event(defines.events.on_built_entity,
   function(event)
     local player = game.get_player(event.player_index)
@@ -137,35 +165,23 @@ script.on_event(defines.events.on_built_entity,
         if (event["consumed_items"] ~= nil and #event.consumed_items > 0) then
           item = event.consumed_items[1]
         end
-        -- Handle Ghosts here
-        if (event.entity.prototype.group.name == 'other') then
-          if (contains(ignore_group, event.entity.ghost_prototype.group.name)) then
-            if (contains(sub_groups_to_forbid, event.entity.ghost_prototype.subgroup.name)) then
-              destroy_not_legendary(player, event.entity, game, item)
-              return
-            end
-            if (contains(sub_groups_to_check, event.entity.ghost_prototype.subgroup.name)) then
-              if (contains(names_to_check, event.entity.ghost_name)) then
-                destroy_not_legendary(player, event.entity, game, item)
-                return
-              end
-            end
-            return
-          end
-        elseif (contains(ignore_group, event.entity.prototype.group.name)) then
-          if (contains(sub_groups_to_forbid, event.entity.prototype.subgroup.name)) then
+        local entity_name = getEntityName(event.entity)
+        local group_name = getGroupName(event.entity, "group")
+        local subgroup_name = getGroupName(event.entity, "subgroup")
+        if (entity_name == 'burner-mining-drill') then
+          return
+        end
+        if (contains(ignore_group, group_name)) then
+          if (contains(sub_groups_to_forbid, subgroup_name)) then
             destroy_not_legendary(player, event.entity, game, item)
             return
           end
-          if (contains(sub_groups_to_check, event.entity.prototype.subgroup.name)) then
-            if (contains(names_to_check, event.entity.name)) then
+          if (contains(sub_groups_to_check, subgroup_name)) then
+            if (contains(names_to_check, entity_name)) then
               destroy_not_legendary(player, event.entity, game, item)
               return
             end
           end
-          return
-        else
-          destroy_not_legendary(player, event.entity, game, item)
           return
         end
         destroy_not_legendary(player, event.entity, game, item)
